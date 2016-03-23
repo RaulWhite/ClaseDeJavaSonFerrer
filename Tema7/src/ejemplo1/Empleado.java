acaca55e1e37363e231f58a8accfecb8620e4ced
Raúl Blanco <raulblanco@example.com>
2016-03-23 15:14:23 +0100

    Día 18/03/2016
    Clases heredadas y ejercicios de recursividad

---
+++ Tema7/src/ejemplo1/Empleado.java
@@ -0,0 +1,22 @@
+package ejemplo1;
+/**
+ * @author Raúl Blanco Rama
+ */
+public class Empleado extends Persona {
+
+	private int categoriaEmpleado;
+
+	public int getCategoriaEmpleado() {
+		return categoriaEmpleado;
+	}
+
+	public void setCategoriaEmpleado(int categoriaEmpleado) {
+		this.categoriaEmpleado = categoriaEmpleado;
+	}
+
+	@Override
+	public void mostrar() {
+		super.mostrar();
+		System.out.println("Categoría de empleado: " + categoriaEmpleado);
+	}
+}
