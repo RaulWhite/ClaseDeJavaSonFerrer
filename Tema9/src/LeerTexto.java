39cf0d037f2483dd4c9d37768924d7e79a54e987
Raúl Blanco Rama <rbr007.movil@gmail.com>
2016-05-31 20:54:05 +0200

    Día 31/05/2016
    Tema9: Lectura y escritura con Buffers
           Cifrado de archivos (byte más)
           Streams de texto (FileReader y BufferedReader)

---
+++ Tema9/src/LeerTexto.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+/**
+ * @author Raúl Blanco Rama
+ */
+
+public class LeerTexto {
+
+	public static void main(String[] args) {
+		
+		leer("src/Bufferes.java");
+		
+	}
+	
+	public static void leer(String nombreArchivo) {
+		
+		try {
+			
+			FileReader archivo = new FileReader(nombreArchivo);
+			BufferedReader buffer = new BufferedReader(archivo);
+			
+			String linea = "" ;
+			boolean eof = false;
+			
+			while (!eof) {
+				linea = buffer.readLine();
+				
+				if (linea == null){
+					eof = true;
+				} else {
+					System.out.println(linea);
+				}
+			}
+			
+			buffer.close();
+			
+		} catch (IOException ioe) {
+			System.out.println("Error de E/S");
+		}
+		
+	}
+	
+}
